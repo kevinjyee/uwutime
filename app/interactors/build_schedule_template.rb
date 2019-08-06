5ba8b2f49faf7e131c949217e9bbdf4371030ad5
kevinjyee <kevinyee@utexas.edu>
2019-08-06 09:44:00 -0400

    set background colors

--- app/interactors/build_schedule_template.rb
+++ app/interactors/build_schedule_template.rb
@@ -4,6 +4,34 @@ class BuildScheduleTemplate
   delegate :recipe, to: :context
   # * context.recipe
 
+  COLOR_PICKER = {
+      'RecipeMashStep' => {
+          backgroundColor: '#17E9E0',
+          color: '#fff',
+          background: '#17E9E0',
+          border: '#adc6ff',
+          borderStyle: 'solid',
+          borderRadius: '10px'
+
+      },
+      'RecipeFermentStep' => {
+          color: '#fff',
+          background: '#ffb48f',
+          backgroundColor:'#ffb48f',
+          border: '#b7eb8f',
+          borderStyle: 'solid',
+          borderRadius:'10px'
+      },
+      'RecipePackagingStep' => {
+          backgroundColor:'#fccd04',
+          color: '#fff',
+          background: '#fccd04',
+          border: '#ffe58f',
+          borderStyle: 'solid',
+          borderRadius:'10px'
+      }
+  }
+
   def call
     context.result = calculate_templates
   end
@@ -38,10 +66,14 @@ class BuildScheduleTemplate
     maxHours = -1
     totalHours = 0
 
-
     recipe.steps.flatten.each do |recipe_step|
       children_steps[recipe_step.display_name] = {
-          hours: recipe_step.duration_hours
+          hours: recipe_step.duration_hours,
+          backgroundColor: COLOR_PICKER[recipe_step.class.name][:backgroundColor],
+          color: COLOR_PICKER[recipe_step.class.name][:color],
+          background: COLOR_PICKER[recipe_step.class.name][:background],
+          border: COLOR_PICKER[recipe_step.class.name][:border],
+          borderRadius: COLOR_PICKER[recipe_step.class.name][:borderRadius]
       }
 
       totalHours += recipe_step.duration_hours
@@ -51,21 +83,27 @@ class BuildScheduleTemplate
       end
 
       if recipe_step.name == 'Transfer'
-        tasks << build_children(children_steps, maxChild, totalHours)
+        tasks << build_task(children_steps, maxChild, totalHours)
         children_steps = {}
         maxChild = nil
         maxHours = -1
         totalHours = 0
+        next
       end
+      tasks << build_task(children_steps, maxChild, totalHours)
     end
     tasks
   end
 
-  def build_children(children_steps, maxChild, totalHours)
+  def build_task(children_steps, maxChild, totalHours)
     {
       maxChild: maxChild,
+      start: Time.now(),
+      end: Time.now() + totalHours*60*60,
       totalHours: totalHours,
-      children: children_steps
+      children: children_steps,
+      title: "#{recipe.name}-#{recipe.id}"
+
     }
   end
 end
